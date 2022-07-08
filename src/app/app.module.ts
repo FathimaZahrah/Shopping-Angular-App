6748fef7e75ade2bba181240a2d3e71112007852
FathimaZahrah <zahrahshafik13@gmail.com>
2022-07-08 12:38:50 +0530

    view product added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ViewproductComponent } from './viewproduct/viewproduct.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ViewproductComponent
   ],
   imports: [
     BrowserModule,
